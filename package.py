37d021670d5ddbc4e2253ce7607c2333a48c971d
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-08-26 13:52:54 -0400

    adding phony paths for testing

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from chip.packages import Package, wrap_install
+from chip.packages import Package, wrap_install, wrap_default
 
 class ConfigPackage(Package):
     def __init__(self, *args, **kwargs):
@@ -10,6 +10,14 @@ class ConfigPackage(Package):
     def install(self):
         with self.indir(self.build_path):
             self.run(['cp', './environment', '/pipeline'])
+
+    @wrap_default('activate')
+    def activate(self):
+        self.path_push('hahlol', "CONF") 
+
+    @wrap_default('deactivate')
+    def deactivate(self):
+        self.path_pull('hahlol', "CONF") 
 
 pk = ConfigPackage("config-pipeline@1.0.0")
 pk.main()
