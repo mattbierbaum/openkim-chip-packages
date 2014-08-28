9bc67c3ec5f7d15045be61db38069118ebe7fc78
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-08-28 11:48:03 -0400

    adding rest of uuid from previous setup (notes mainly)

--- package.py
+++ package.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
+import os
+import uuid
+import subprocess
 from chip.packages import Package, wrap_install, wrap_default
 
 class ConfigPackage(Package):
     def __init__(self, *args, **kwargs):
         super(ConfigPackage, self).__init__(*args, **kwargs)
+        self.home = "/pipeline"
 
     @wrap_install
     def install(self):
+        if not os.path.exists(self.home):
+            subprocess.check_call(['sudo', 'mkdir', '-p', self.home])
+
         with self.indir(self.build_path):
-            self.run(['cp', './environment', '/pipeline'])
+            self.run(['cp', './environment', self.home])
+
+        u = str(uuid.uuid4())
+
+        with open(os.path.join(self.home, 'environment.extra'), 'w') as f:
+            f.write("UUID="+u)
 
     @wrap_default('activate')
     def activate(self):
-        self.path_push('hahlol', "CONF") 
+        pass
 
     @wrap_default('deactivate')
     def deactivate(self):
-        self.path_pull('hahlol', "CONF") 
+        pass
 
 pkg = ConfigPackage
